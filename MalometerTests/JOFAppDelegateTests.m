99b6c82a028272b14f67e56e601de49b4f7cc94a
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2014-06-26 20:36:00 +0200

    Added two more tests to the app delegate. One that tests state and another that tests behavior.

--- MalometerTests/JOFAppDelegateTests.m
+++ MalometerTests/JOFAppDelegateTests.m
@@ -12,6 +12,26 @@
 #import "JOFAppDelegate.h"
 
 
+@interface MocFake : NSManagedObjectContext
+
+@property (assign) BOOL saveWasCalled;
+
+@end
+
+@implementation MocFake
+
+- (BOOL) hasChanges {
+    return YES;
+}
+
+- (BOOL) save:(NSError *__autoreleasing *)error {
+    self.saveWasCalled = YES;
+    return YES;
+}
+
+@end
+
+
 @interface JOFAppDelegateTests : XCTestCase {
     // Object to test.
     JOFAppDelegate *sut;
@@ -57,6 +77,29 @@
 
     // Check
     XCTAssertNotNil(sut, @"The object to test must be created in setUp.");
+}
+
+
+#pragma mark - Core Data stack
+
+- (void) testManagedObjectContextIsCreatedInAccessor {
+    // Operate
+    NSManagedObjectContext *moc = [sut managedObjectContext];
+
+    // Check
+    XCTAssertNotNil(moc, @"Managed object context must be created in accessor.");
+}
+
+
+- (void) testSaveContextTellsMocToSave {
+    // Prepare
+    MocFake *mocFake = [[MocFake alloc] init];
+    [sut setValue:mocFake forKeyPath:@"managedObjectContext"];
+
+    // Operate
+    [sut saveContext];
+
+    XCTAssertTrue(mocFake.saveWasCalled, @"Data must be saved using the managed object context.");
 }
 
 @end
